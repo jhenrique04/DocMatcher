2e44794ac4c24c2025d5aa4088b9f103fd8dfe38
jhenrique04 <jhenriquevs.04@gmail.com>
2023-10-22 21:03:35 -0300

    feat: switch to pdfminer

--- pdf_reader.py
+++ pdf_reader.py
@@ -1,10 +1,6 @@
-import PyPDF4
+from pdfminer.high_level import extract_text
 
 class PDFReader:
-    def extract_text(self, file_path):
-        with open(file_path, 'rb') as file:
-            reader = PyPDF4.PdfFileReader(file)
-            text = ""
-            for page in range(reader.numPages):
-                text += reader.getPage(page).extractText()
-        return text
+    @staticmethod
+    def extract_text(file_path):
+        return extract_text(file_path)
