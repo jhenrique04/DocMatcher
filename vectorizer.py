fc2ba0cc274017d6365351baf49ef7c3beec8d5d
jhenrique04 <jhenriquevs.04@gmail.com>
2023-10-22 23:34:40 -0300

    feat: calculate similarity using gensim cossim function

--- vectorizer.py
+++ vectorizer.py
@@ -1,17 +1,16 @@
-import numpy as np
 from gensim.models import KeyedVectors
-from sklearn.metrics.pairwise import cosine_similarity
+from gensim.matutils import cossim
+import numpy as np
 
 class Vectorizer:
-    def __init__(self, model_path):
-        self.model = KeyedVectors.load_word2vec_format(model_path, unicode_errors='ignore')
+    def __init__(self, word_embedding_path):
+        self.model = KeyedVectors.load_word2vec_format(word_embedding_path)
+    
+    def calculate_document_vector(self, text):
+        vectors = [self.model[word] for word in text if word in self.model.vocab]
+        if not vectors:
+            return np.zeros(self.model.vector_size)
+        return np.mean(vectors, axis=0)
 
-    def calculate_document_vector(self, processed_text):
-        vectors = [self.model[word] for word in processed_text if word in self.model.key_to_index]
-        if vectors:
-            return np.mean(vectors, axis=0)
-        return np.zeros(self.model.vector_size)
-
-    def calculate_similarity(self, doc_vector, all_vectors):
-        similarities = cosine_similarity([doc_vector], all_vectors)
-        return similarities[0]
+    def calculate_similarity(self, doc_vector, other_vector):
+        return cossim(doc_vector, other_vector)
